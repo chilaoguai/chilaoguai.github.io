3b01edf1cb700197ffa142d21a8ce3c098bc5608
chilaoguai <936056762@qq.com>
2023-10-24 23:51:39 +0800

    Site updated: 2023-10-24 23:51:38

--- anzhiyu/random.js
+++
@@ -1,1 +0,0 @@
-var posts=["2023/10/23/hexo创建分类的方法/","2023/10/23/hexo创建“标签”的方法/","2023/10/23/《老怪笔记》博客上线了/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
